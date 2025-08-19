292ca1a3de6949d067492c98b2f9ad820b124ee1
Srikanth-vlsi13 <srikanth720440@gmail.com>
2025-08-19 17:42:48 +0530

    Update Adder_payload.h

--- Adder_using_TLM2.0 /Adder_payload.h
+++ Adder_using_TLM2.0 /Adder_payload.h
@@ -1,10 +1,16 @@
-#ifndef ADDER_PAYLOAD_H
+#ifndef ADDER_PAYLOAD_H   // Header guard start: prevents multiple inclusion of this file
 #define ADDER_PAYLOAD_H
 
+// -----------------------------
+// Custom Payload
+// -----------------------------
+// This struct defines the data structure that will be carried inside TLM transactions.
+// It holds the inputs (a, b) and the result (sum) for the adder operation.
 struct AdderPayload {
-    int a;
-    int b;
-    int sum;
+    int a;    // first operand
+    int b;    // second operand
+    int sum;  // result (computed by target)
 };
 
-#endif // ADDER_PAYLOAD_H
+#endif // ADDER_PAYLOAD_H   // Header guard end
+
